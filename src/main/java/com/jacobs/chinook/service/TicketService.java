5e8ddcdc113fd7cb66fddd848a9253a7acdd92d0
ChrisJacobsJr <bluflare12345@gmail.com>
2026-02-18 01:05:09 -0500

    finished defining service

--- src/main/java/com/jacobs/chinook/service/TicketService.java
+++ src/main/java/com/jacobs/chinook/service/TicketService.java
@@ -3,11 +3,14 @@ package com.jacobs.chinook.service;
 /**
  * Manipulates Tickets and Messages
  * Retrieves info about Tickets and Messages
+ *
+ * Note: Use Spring Security if possible
  */
 public class TicketService {
-    // boolean 5createTicket(userId, ?...)
+    // boolean createTicket(userId, ?...)
     // boolean closeTicket(userId, ticketId)
     // boolean reopenTicket(userId, ticketId)
     // boolean createMessage(userId, ticketId)
+    // some_DTO getMessageInfo(userId, ticketId)
 
 }
