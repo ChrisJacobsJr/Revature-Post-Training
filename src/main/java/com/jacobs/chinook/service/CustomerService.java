adbed7f8ac58b7b9871653ba57b31174d828ded7
ChrisJacobsJr <bluflare12345@gmail.com>
2026-02-18 10:30:36 -0500

    added methods for customer and employee services. Now to do TicketService, repository methods, and the remaining entities

--- src/main/java/com/jacobs/chinook/service/CustomerService.java
+++ src/main/java/com/jacobs/chinook/service/CustomerService.java
@@ -1,11 +1,56 @@
 package com.jacobs.chinook.service;
 
+import com.jacobs.chinook.entity.InvoiceLine;
+import com.jacobs.chinook.entity.Track;
+import com.jacobs.chinook.repository.CustomerRepository;
+import com.jacobs.chinook.repository.InvoiceLineRepository;
+import com.jacobs.chinook.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Retrieves info about/related to customers
  */
+@Service
 public class CustomerService {
-    // List<Track> getOwnedTracks(customerId)
-    // List<InvoiceLine> getCustomerInvoices(customerId)
-    // double(?) getCustomerTotalSpending(customerId)
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private TrackRepository trackRepository;
+    @Autowired
+    private InvoiceLineRepository invoiceLineRepository;
+
+    /**
+     * Returns a list of Track entities that a customer has a license for.
+     * TODO: check to see if I should implement authentication here (through Spring Security) based on best practices
+     * @param customerId
+     * @return a list of Track SpringBoot @Entity's
+     */
+    public List<Track> getOwnedTracks(Integer customerId) {
+        return trackRepository.getOwnedTracksByCustomerId(customerId);
+    }
+
+    /**
+     * Returns a list of InvoiceLine entities for the purpose of viewing a given customer's purchases
+     * TODO: see if authentication here would be best practice, or an unnecessary limitation
+     * @param customerId
+     * @return a list of InvoiceLine @Entity's
+     */
+    public List<InvoiceLine> getCustomerPurchases(Integer customerId) {
+        return invoiceLineRepository.getCustomerPurchases(customerId);
+    }
+
+    /**
+     * Returns the total spending for a given customer
+     * TODO: see above
+     * @param customerId
+     * @return a double representing the total expenditure of a given customer
+     */
+    public double getCustomerTotalSpending(Integer customerId) {
+        return invoiceLineRepository.getCustomerTotalSpending(customerId);
+    }
+
 
 }
--- src/main/java/com/jacobs/chinook/service/EmployeeService.java
+++ src/main/java/com/jacobs/chinook/service/EmployeeService.java
@@ -1,9 +1,23 @@
 package com.jacobs.chinook.service;
 
+import com.jacobs.chinook.entity.Customer;
+import com.jacobs.chinook.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Retrieves info about/related to employees
  */
+@Service
 public class EmployeeService {
-    // listAssistedCustomers(employeeId)
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Customer> listAssistedCustomers(Integer employeeId) {
+        return customerRepository.getAssistedCustomers(employeeId);
+    }
+
 
 }
