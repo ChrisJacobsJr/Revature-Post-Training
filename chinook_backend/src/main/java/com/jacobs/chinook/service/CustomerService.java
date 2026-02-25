467bb786bd97676c18c8e1e506f67bb27498a02a
ChrisJacobsJr <bluflare12345@gmail.com>
2026-02-25 01:58:12 -0500

    added some comments to fence development

--- chinook_backend/src/main/java/com/jacobs/chinook/service/CustomerService.java
+++ chinook_backend/src/main/java/com/jacobs/chinook/service/CustomerService.java
@@ -25,34 +25,33 @@ public class CustomerService {
 
     /**
      * Returns a list of Track entities that a customer has a license for.
-     * TODO: check to see if I should implement authentication here (through Spring Security) based on best practices
      * @param customerId
      * @return a list of Track SpringBoot @Entity's
      */
     public List<Track> getOwnedTracks(Integer customerId) {
-        List<Track> tracks = trackRepository.getOwnedTracksByCustomerId(customerId)
-        return null;
+        // Data isn't particularly sensitive, so we'll skip the DTO and auth
+        return trackRepository.getOwnedTracksByCustomerId(customerId);
     }
 
     /**
      * Returns a list of InvoiceLine entities for the purpose of viewing a given customer's purchases
-     * TODO: see if authentication here would be best practice, or an unnecessary limitation
-     * todo alternatively, make it so that the DTO changes depending on who the user is
      * @param customerId
      * @return a list of InvoiceLine @Entity's
      */
     public List<InvoiceLine> getCustomerPurchases(Integer customerId) {
+        // try to make it so that either the owning customer or an employee can call this function
         List<InvoiceLine> purchases = invoiceLineRepository.getCustomerPurchases(customerId);
+        // convert these into a DTO and save
         return null;
     }
 
     /**
      * Returns the total spending for a given customer
-     * TODO: add an annotation so only employees can access this
      * @param customerId
      * @return a double representing the total expenditure of a given customer
      */
     public double getCustomerTotalSpending(Integer customerId) {
+        // add security such that only the owning customer or an employee can access this
         return invoiceLineRepository.getCustomerTotalSpending(customerId);
     }
 
